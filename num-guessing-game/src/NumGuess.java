c3e01f858e52ecd2d3d0c47245f85602bbcde4b6
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-16 19:22:02 +0800

    implement weird die method with basic guessing functionality

--- num-guessing-game/src/NumGuess.java
+++ num-guessing-game/src/NumGuess.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class NumGuess {
     public static void main() {
@@ -8,17 +9,50 @@ public class NumGuess {
         Scanner scanner = new Scanner(System.in);
 
         // Ask user for choice of game
-        System.out.print("Would you like to guess a number from the normal die (numbers 1-6) or the weird die (random set of 6 numbers from 1 to 100)? "
+        System.out.print("Would you like to guess a number from the normal die (numbers 1-6) or the weird die (random set of 10 numbers from 1 to 100)? "
                 + "\n" + "Type '1' for normal die, or '2' for weird die: ");
         int choice = scanner.nextInt();
 
         // MAIN GAME
         if (choice == 1) {
             normalDie(random, scanner);
+        } else if (choice == 2) {
+            weirdDie(random, scanner);
         }
+
+        scanner.close();
     }
     private static void normalDie(Random random, Scanner scanner) {
         int die_result = random.nextInt(1, 7);
+        int ans;
+
+        // Game will re-run until correct guess then break out of loop
+        do {
+            System.out.print("Pick a number: ");
+            ans = scanner.nextInt();
+            if (ans!= die_result) {
+                System.out.println("Wrong answer, guess again!");
+            }
+        } while (ans != die_result);
+        System.out.println("CORRECT GUESS!");
+    }
+
+    private static void weirdDie(Random random, Scanner scanner) {
+        // initialise empty array of size 10
+        int[] weirdDie = new int[10];
+
+        // create weird die with 10 numbers
+        for (int i = 0; i < weirdDie.length; i++) {
+            weirdDie[i] = random.nextInt(1, 100);
+        }
+
+        // sort array
+        Arrays.sort(weirdDie);
+        System.out.println("weird die: " + Arrays.toString(weirdDie));
+
+        // create guess with index of the die and initialise guess
+        int die_result = weirdDie[random.nextInt(0, 10)];
+        System.out.println("You rolled " + die_result);
         int ans;
 
         // Game will re-run until correct guess then break out of loop
