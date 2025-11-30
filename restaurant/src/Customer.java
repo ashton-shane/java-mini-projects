a48d39b733fe975464ade2b5f89acc68f5b5dd4e
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-30 23:46:05 +0800

    implement working resto MVP

--- restaurant/src/Customer.java
+++ restaurant/src/Customer.java
@@ -1,19 +1,33 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Customer {
     int customer_id;
-    ArrayList<String> orders = new ArrayList<>();
+    ArrayList<String> orders;
 
     Customer(int customer_id) {
         this.customer_id = customer_id;
-        this.orders = null;
+        this.orders = new ArrayList<>();
+        System.out.println("WELCOME NEW CUSTOMER!");
     }
 
     void orderFood(String order){
         this.orders.add(order);
+        System.out.println("Customer "+ this.customer_id + " has ordered " + order);
     }
 
-    void payBill(){
-        System.out.println("Customer " + this.customer_id + "has paid their bill.");
+    float calcBill(){
+        float total = 0.00f;
+        HashMap<String, Float> prices = Menu.getMenu();
+
+        for(String order : this.orders) {
+            total += prices.get(order);
+        }
+        System.out.println("The total bill amounts to: $" + String.format("%.2f",total));
+        return total;
+    }
+    void payBill(float total){
+        System.out.println("Customer " + this.customer_id + " has paid their bill amounting to $" + String.format("%.2f",total));
     }
 }
--- restaurant/src/Menu.java
+++ restaurant/src/Menu.java
@@ -1,27 +1,34 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Menu {
-    HashMap<String, float> menu = new HashMap<>();
+    // private so it can only use the setter methods below
+    private static HashMap<String, Float> menu = new HashMap<>();
 
     /* Using a static initialising block for the menu because I want it loaded when class is loaded.
        It would be better to put this inside the Menu constructor in the instance I have various menus
        but for now, I just want it shared with other classes.
     */
-    {
+    static {
         menu.put("Pancakes", 18.50f);
         menu.put("Hot Chocolate", 7.00f);
         menu.put("Eggs with Sausage", 18.00f);
     }
 
     Menu(){
-
+        // Empty constructor as no need to initialise menu.
     }
 
-    void addItem(){
-        menu.put();
+    // GETTER for menu
+    public static HashMap<String, Float> getMenu(){
+        return menu;
     }
 
-    void removeItem(){
-        menu.remove();
+    public static void addItem(String item, float price){
+        menu.put(item, price);
+    }
+
+    public static void removeItem(String item){
+        menu.remove(item);
     }
 }
