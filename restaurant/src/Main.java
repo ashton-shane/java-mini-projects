eecc80a2f0dff8c22de1785638eac49b0160e17b
Ashton Shane <ashton.shane.dev@gmail.com>
2025-12-01 00:05:58 +0800

    add methods to Restaurant class and allow restaurant name to pass to Customer class

--- restaurant/src/Main.java
+++ restaurant/src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Customer custOne = new Customer(1);
+        // open restaurant
+        Restaurant ashies = new Restaurant("Ashie's");
+        String restoName = ashies.getName();
+        ashies.openRestaurant();
+
+        // customer transactions
+        Customer custOne = new Customer(1, restoName);
         custOne.orderFood("Pancakes");
         custOne.orderFood("Hot Chocolate");
         custOne.orderFood("Eggs with Sausage");
         float bill = custOne.calcBill();
         custOne.payBill(bill);
+
+        // close restaurant
+        ashies.closeRestaurant();
     }
 }
--- restaurant/src/Restaurant.java
+++ restaurant/src/Restaurant.java
@@ -1,9 +1,22 @@
 public class Restaurant {
-    String name;
-    boolean isOpen;
+    private final String name;
+    private boolean isOpen;
 
     Restaurant(String name) {
         this.name = name;
         this.isOpen = false;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void openRestaurant(){
+        this.isOpen = true;
+        System.out.println(name + " is now open!");
+    }
+
+    public void closeRestaurant(){
+        this.isOpen = false;
+        System.out.println(name + " is now closed! See you next time!");
     }
 }
